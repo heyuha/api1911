1c92a848ee70a311cceadb5585b391f3dade38ea
heyuhao <ranhaogg@qq.com>
2020-07-15 03:02:47 +0000

    获取accecc_token

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,4 +9,15 @@ class TestController extends Controller
     public function hello(){
     	echo "hello world";
     }
+
+    //获取access_token 
+    public function wxgettoken(){
+    	$appid = "wxc995cb21dadc2fd8";
+    	$appsecret = "c59aca85f61188b80cf5f824f0b6b6a8";
+    	$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret;
+    	$cont = file_get_contents($url);
+    	$content = json_decode($cont,true);
+    	print_r($content['access_token']);
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get("/hello","TestController@hello");
+Route::get("/wx/token","TestController@wxgettoken");
